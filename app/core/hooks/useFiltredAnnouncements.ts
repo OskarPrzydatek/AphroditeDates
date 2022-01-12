20e454bf0f1261be4ec3124a76f7c0de78a145c9
Oskar <oskar.m.przydatek@gmail.com>
2022-01-12 01:00:42 +0100

    pagination

--- app/core/hooks/useFiltredAnnouncements.ts
+++
@@ -1,7 +0,0 @@
-import getFiltredtredAnnouncements from "app/announcements/queries/getFiltredtredAnnouncements"
-import { useQuery } from "blitz"
-
-export default function useFiltredAnnouncements(values) {
-  const [filtredAnnouncements] = useQuery(getFiltredtredAnnouncements, values)
-  return filtredAnnouncements
-}
